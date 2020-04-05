25752a4027c76b85640a1ae3df55e6da4505dab4
RolandWarburton <warburtonroland@gmail.com>
2020-04-05 23:36:36 +1000

    :bug: Fix typo

--- src/views/Notes/Linux/Terminals.js
+++ src/views/Notes/Linux/Terminals.js
@@ -2,6 +2,6 @@ const page = ``;
 
 module.exports = {
     page: page, 
-	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/Systemd.md"],
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/Terminals.md"],
 	template: "templates/navigablePage.ejs"
 };
