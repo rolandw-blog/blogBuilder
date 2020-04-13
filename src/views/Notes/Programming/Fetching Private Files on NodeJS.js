baef554c8373020fcc74c3a8d16e2c5fcedecf65
RolandWarburton <warburtonroland@gmail.com>
2020-04-13 23:15:01 +1000

    :pencil: Add pages

---
+++ src/views/Notes/Programming/Fetching Private Files on NodeJS.js
@@ -0,0 +1,7 @@
+const page = ``;
+
+module.exports = {
+    page: page, 
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/programming/Fetching private files on NodeJS.md"],
+	template: "templates/navigablePage.ejs"
+};
