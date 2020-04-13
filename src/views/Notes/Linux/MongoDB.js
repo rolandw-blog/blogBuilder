23c9e2e571a5eaa36335acb942439c962f83fcc9
RolandWarburton <warburtonroland@gmail.com>
2020-04-13 20:45:40 +1000

    :pencil: Add MongoDB notes page

---
+++ src/views/Notes/Linux/MongoDB.js
@@ -0,0 +1,7 @@
+const page = ``;
+
+module.exports = {
+    page: page, 
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/MongoDB.md"],
+	template: "templates/navigablePage.ejs"
+};
