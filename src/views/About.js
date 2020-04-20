2a6ef99dd4b833204df6524c099399273c6bf147
RolandWarburton <warburtonroland@gmail.com>
2020-04-20 19:15:24 +1000

    :rocket: Added about page

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,27 @@
 const listFilesInDirPartial = require('../../templates/partials/listFilesInDirPartial')
 
 const page = `
-${listFilesInDirPartial("src/views/Notes")}
+<div class='imageAndTextWrapper'>
+    <aside>
+        <p>
+            I am a Computer enthusiast writing software and building things.
+        </p>
+        <br />
+        <p>
+            My interests are hacking, automation, and talking about software design philosophy. 
+            My career goal is to work in devops / system administration by day and hack computers by night.
+        </p>
+        <br />
+        <p>
+            I am studying a BA of Computer Science in cybersecurity and network design at a university in Melbourne Australia.
+        </p>
+    </aside>
+    <svg class='avatar'/>
+</div>
 `;
 
 module.exports = {
-    page: page, 
+	page: page,
 	target: null,
 	template: null
 };
