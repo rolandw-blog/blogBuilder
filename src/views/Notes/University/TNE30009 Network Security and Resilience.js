5018546477c6f1ea1c493bfb8b1073fcd9549ccb
RolandWarburton <warburtonroland@gmail.com>
2020-04-20 19:04:17 +1000

    :pencil: Add more uni notes

--- src/views/Notes/University/TNE30009 Network Security and Resilience.js
+++ src/views/Notes/University/TNE30009 Network Security and Resilience.js
@@ -7,12 +7,13 @@ const W2L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2
 const W2L2 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W2/W2_Lecture2.md"
 const W3L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W3/W3_Lecture1.md"
 const W3L2 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W3/W3_lecture2.md"
-const W4L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_lecture1.md"
-const W5L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W5/W5_Lecture1.md"
-const W5L2 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W5/W5_Lecture2.md"
+const W4L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_Lecture1.md"
+const W4L2 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_Lecture2.md"
+const W4L3 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_Lecture3.md"
+const W5L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W5/W5_Lab.md"
 
 module.exports = {
     page: page, 
-	target: [W1L1, W1L2, W2L1, W2L2, W3L1, W3L2, W4L1, W5L1, W5L2],
+	target: [W1L1, W1L2, W2L1, W2L2, W3L1, W3L2, W4L1, W4L2, W4L3, W5L1],
 	template: "templates/navigablePage.ejs"
 };
