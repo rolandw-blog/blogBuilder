d3feed37e62b6327bd16cf67ca0cc732c4a57c17
RolandWarburton <warburtonroland@gmail.com>
2020-04-21 16:18:36 +1000

    :pencil: Add more content sources

--- src/views/Notes/University/TNE30009 Network Security and Resilience.js
+++ src/views/Notes/University/TNE30009 Network Security and Resilience.js
@@ -11,9 +11,10 @@ const W4L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2
 const W4L2 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_Lecture2.md"
 const W4L3 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W4/W4_Lecture3.md"
 const W5L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W5/W5_Lab.md"
+const W6L1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S1/master/TNE30009_Network_Security_and_Resilience/W6/W6_Lecture1.md"
 
 module.exports = {
     page: page, 
-	target: [W1L1, W1L2, W2L1, W2L2, W3L1, W3L2, W4L1, W4L2, W4L3, W5L1],
+	target: [W1L1, W1L2, W2L1, W2L2, W3L1, W3L2, W4L1, W4L2, W4L3, W5L1, W6L1],
 	template: "templates/navigablePage.ejs"
 };
