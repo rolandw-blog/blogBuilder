bdf8719e18956fba5baac521a889b609c231aa47
RolandWarburton <warburtonroland@gmail.com>
2020-04-24 14:53:11 +1000

    :pencil: Add more content and media

---
+++ src/views/Notes/Networking/Topologies.js
@@ -0,0 +1,11 @@
+const page = `
+`;
+
+const content = "https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Networking/Topologies.md" 
+
+
+module.exports = {
+    page: page, 
+	target: [],
+	template: "templates/navigablePage.ejs"
+};
