0a90fba993b5bb60464ed864df1942be7bd4ac51
RolandWarburton <warburtonroland@gmail.com>
2020-04-26 18:41:40 +1000

    :pencil: Add networking notes

---
+++ src/views/Notes/Networking/Media Types and Standards.js
@@ -0,0 +1,11 @@
+const page = `
+`;
+
+const content = "https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Networking/Media Types and Standards.md" 
+
+
+module.exports = {
+    page: page, 
+	target: [content],
+	template: "templates/navigablePage.ejs"
+};
---
+++ src/views/Notes/Networking/Network Components.js
@@ -0,0 +1,11 @@
+const page = `
+`;
+
+const content = "https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Networking/Network Components.md" 
+
+
+module.exports = {
+    page: page, 
+	target: [content],
+	template: "templates/navigablePage.ejs"
+};
