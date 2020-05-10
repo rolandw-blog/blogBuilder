e4e3eb22ae16fcfe1015d1a55f284cbc8165ec1b
RolandWarburton <warburtonroland@gmail.com>
2020-05-11 00:01:46 +1000

    :pencil: Add nginx notes

---
+++ src/views/Notes/Linux/nginx.js
@@ -0,0 +1,7 @@
+const page = ``;
+
+module.exports = {
+    page: page, 
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/nginx.md"],
+	template: "templates/navigablePage.ejs"
+};
