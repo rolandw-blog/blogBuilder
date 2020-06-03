faa10d3778f0a10d67ea1b169ec7b70c395e7403
RolandWarburton <warburtonroland@gmail.com>
2020-06-03 23:54:59 +1000

    :Pencil: Add content

---
+++ src/views/Notes/Networking/Ubiquiti Log.js
@@ -0,0 +1,11 @@
+const page = `
+`;
+
+const content = "https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Networking/Ubiquiti Log.md" 
+
+
+module.exports = {
+    page: page, 
+	target: [content],
+	template: "templates/navigablePage.ejs"
+};
