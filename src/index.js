faccae754a38dc7094f055bf496ac2da3704c439
RolandWarburton <warburtonroland@gmail.com>
2020-06-09 17:42:21 +1000

    :outbox_tray: Add github social link

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 	document.querySelector(".caret").addEventListener("mousedown", () => {
-
-
 		// create the social box
-		const socialBox = document.createElement("div")
-		socialBox.className = "socialBox"
+		const socialBox = document.createElement("div");
+		socialBox.className = "socialBox";
 
-		socialBox.appendChild(createSocialIcon("/media/twitter.svg", "https://twitter.com/RolandIRL"))
-		socialBox.appendChild(createSocialIcon("/media/linkedin.svg", "https://www.linkedin.com/in/roland-w/"))
+		socialBox.appendChild(
+			createSocialIcon(
+				"/media/twitter.svg",
+				"https://twitter.com/RolandIRL"
+			)
+		);
+		socialBox.appendChild(
+			createSocialIcon(
+				"/media/linkedin.svg",
+				"https://www.linkedin.com/in/roland-w/"
+			)
+		);
+		socialBox.appendChild(
+			createSocialIcon(
+				"/media/github.svg",
+				"https://github.com/RolandWarburton"
+			)
+		);
 
 		// get the little spinny logo thing
 		const caret = event.target;
@@ -20,20 +34,18 @@ document.addEventListener("DOMContentLoaded", () => {
 			event.target.parentNode.parentNode.appendChild(socialBox);
 			caret.setAttribute("id", "caret-flip");
 		}
+	});
+});
 
-
-	})
-})
-
-const createSocialIcon = function(imagePath, socialLink) {
+const createSocialIcon = function (imagePath, socialLink) {
 	const socialIconAnchor = document.createElement("a");
 	socialIconAnchor.href = socialLink;
-	socialIconAnchor.className = "darkHyperLink"
+	socialIconAnchor.className = "darkHyperLink";
 
 	const socialIconImage = document.createElement("img");
 	socialIconImage.src = imagePath;
 
-	socialIconAnchor.appendChild(socialIconImage)
+	socialIconAnchor.appendChild(socialIconImage);
 
 	return socialIconAnchor;
-}
+};
