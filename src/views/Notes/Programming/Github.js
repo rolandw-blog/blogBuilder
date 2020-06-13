bc57571edd6651a28c2e28c3b243377273a943de
RolandWarburton <warburtonroland@gmail.com>
2020-06-14 01:47:24 +1000

    :pencil: Add content

---
+++ src/views/Notes/Programming/Github.js
@@ -0,0 +1,7 @@
+const page = ``;
+
+module.exports = {
+	page: page,
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/programming/Github.md"],
+	template: "templates/navigablePage.ejs"
+};
