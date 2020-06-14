949ea7b0b897200e1275df3f88300e2d1326ac40
RolandWarburton <warburtonroland@gmail.com>
2020-06-14 21:22:27 +1000

    :pencil: ADD Content

---
+++ src/views/Writing/Project Ideas.js
@@ -0,0 +1,8 @@
+const page = `
+`;
+
+module.exports = {
+	page: page,
+	target: ["https://gist.githubusercontent.com/RolandWarburton/d1709ec27e60333303e87a6ef4c783ab/raw"],
+	template: "templates/navigablePage.ejs"
+};
