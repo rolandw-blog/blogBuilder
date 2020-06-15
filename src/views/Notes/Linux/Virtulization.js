edf5cfd79c1760d138f685cf0ef4339a72d8399d
RolandWarburton <warburtonroland@gmail.com>
2020-06-15 20:01:43 +1000

    :pencil: ADD content

---
+++ src/views/Notes/Linux/Virtulization.js
@@ -0,0 +1,9 @@
+const page = ``;
+
+module.exports = {
+	page: page,
+	target: [
+		"https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/Virtualization.md",
+	],
+	template: "templates/navigablePage.ejs",
+};
