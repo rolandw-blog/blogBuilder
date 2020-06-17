6fec6074973f8cc2a97e8f7f68c0985156cdaf0b
RolandWarburton <warburtonroland@gmail.com>
2020-06-17 19:00:17 +1000

    :pencil: ADD Content

---
+++ src/views/Notes/Linux/LAMP & WordPress.js
@@ -0,0 +1,7 @@
+const page = ``;
+
+module.exports = {
+	page: page,
+	target: ["https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/LAMP & WordPress.md"],
+	template: "templates/navigablePage.ejs"
+};
