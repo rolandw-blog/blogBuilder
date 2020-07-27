932fb1a23bff456f3b13f4f5ab600c0ee07b6098
RolandWarburton <warburtonroland@gmail.com>
2020-07-28 01:51:28 +1000

    :pencil: ADD content

---
+++ src/views/Notes/Programming/Deploying docker websites.js
@@ -0,0 +1,9 @@
+const page = ``;
+
+module.exports = {
+	page: page,
+	target: [
+		"https://raw.githubusercontent.com/RolandWarburton/knowledge/master/programming/Deploying docker websites.md",
+	],
+	template: "templates/navigablePage.ejs",
+};
