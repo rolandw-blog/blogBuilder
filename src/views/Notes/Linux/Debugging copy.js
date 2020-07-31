143cffcc54c7fee942812b2a74415b1f52b73485
RolandWarburton <warburtonroland@gmail.com>
2020-08-01 00:36:33 +1000

    :pencil: Add content

---
+++ src/views/Notes/Linux/Debugging copy.js
@@ -0,0 +1,9 @@
+const page = ``;
+
+module.exports = {
+	page: page,
+	target: [
+		"https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Linux/Host Names.md",
+	],
+	template: "templates/navigablePage.ejs",
+};
