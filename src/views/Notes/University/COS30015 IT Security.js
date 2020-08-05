4af2d48291d07b5f2dd41e3c63da685908a20806
RolandWarburton <warburtonroland@gmail.com>
2020-08-05 20:36:27 +1000

    :pencil: ADD Content

---
+++ src/views/Notes/University/COS30015 IT Security.js
@@ -0,0 +1,10 @@
+const page = `
+`;
+
+const W1LAB1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/COS30015-IT-Security/week1/labs/lab.md" 
+
+module.exports = {
+    page: page, 
+	target: [W1LAB1],
+	template: "templates/navigablePage.ejs"
+};
