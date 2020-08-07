e1dbefae6cfe11f64d4b6676709ac52627c46f03
RolandWarburton <warburtonroland@gmail.com>
2020-08-07 23:24:40 +1000

    :pencil: ADD Content

---
+++ src/views/Notes/University/TNE30019 Unix for Telecommunications.js
@@ -0,0 +1,10 @@
+const page = `
+`;
+
+const LAB1 = "raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/TNE80014-Unix%20for%20Telecommunications/week1/labs/lab1.md"
+
+module.exports = {
+    page: page, 
+	target: [LAB1],
+	template: "templates/navigablePage.ejs"
+};
