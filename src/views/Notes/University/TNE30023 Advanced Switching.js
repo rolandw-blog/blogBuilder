da2767f02f85b4e738ee11671500622bfe52c1cb
RolandWarburton <warburtonroland@gmail.com>
2020-08-09 00:27:04 +1000

    :pencil: ADD Content

--- src/views/Notes/University/TNE30023 Advanced Switching.js
+++ src/views/Notes/University/TNE30023 Advanced Switching.js
@@ -2,9 +2,10 @@ const page = `
 `;
 
 const SETUP = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/TNE30023-Advanced-Switching/README.md"
+const LAB1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/TNE30023-Advanced-Switching/week1/labs/lab1.md"
 
 module.exports = {
     page: page, 
-	target: [SETUP],
+	target: [SETUP, LAB1],
 	template: "templates/navigablePage.ejs"
 };
