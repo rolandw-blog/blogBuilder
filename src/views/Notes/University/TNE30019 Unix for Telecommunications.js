5bd5feb439bae684e0d9f618a0213010d17e5110
RolandWarburton <warburtonroland@gmail.com>
2020-08-12 13:56:49 +1000

    :bug: FIX link

--- src/views/Notes/University/TNE30019 Unix for Telecommunications.js
+++ src/views/Notes/University/TNE30019 Unix for Telecommunications.js
@@ -1,7 +1,7 @@
 const page = `
 `;
 
-const LAB1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/TNE80014-Unix%20for%20Telecommunications/week1/labs/lab1.md"
+const LAB1 = "https://TOKEN@raw.githubusercontent.com/RolandWarburton/Swinburne2020S2/master/TNE80014-Unix for Telecommunications/week1/labs/lab1.md"
 
 module.exports = {
     page: page, 
