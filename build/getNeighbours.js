45643a0512e8aed9c633bf3897e5d5d2dad62300
RolandWarburton <warburtonroland@gmail.com>
2020-08-16 15:21:20 +1000

    :sparkles: Add more data to template neighbours

--- build/getNeighbours.js
+++ build/getNeighbours.js
@@ -7,21 +7,25 @@ const path = require("path");
  * @example getNeighbours(['page1', 'page2'], {websitePath: '/path/pages'})
  */
 const getNeighbours = (siblings, page) => {
+	// get the parent of this page to find its index
 	const parent = getParent(page.websitePath);
+	// get the name (last item in the websitePath)
 	const lastPathName = path.parse(page.websitePath).name;
 
 	const l = siblings.length;
-	const result = {};
+	const result = { prev: {}, next: {} };
 
 	const nIndex = siblings.indexOf(lastPathName) + 1;
 	const pIndex = siblings.indexOf(lastPathName) - 1;
 
 	if (nIndex < l && nIndex >= 0) {
-		result.next = `${parent}/${siblings[nIndex]}`;
+		result.next.path = `${parent}/${siblings[nIndex]}`;
+		result.next.name = siblings[nIndex];
 	}
 
 	if (pIndex < l && pIndex >= 0) {
-		result.prev = `${parent}/${siblings[pIndex]}`;
+		result.prev.path = `${parent}/${siblings[pIndex]}`;
+		result.prev.name = siblings[pIndex];
 	}
 
 	return result;
