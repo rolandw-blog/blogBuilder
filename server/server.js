2ddfa147c6660ebef2d8e1819677f664e958ca61
RolandWarburton <warburtonroland@gmail.com>
2020-08-17 21:55:42 +1000

    :sparkles: Finish implementing buildPage

--- server/server.js
+++ server/server.js
@@ -6,9 +6,20 @@ const debug = require("debug")("staticFolio:server");
 const verifyPayload = require("./middleware/verifyPayload");
 const build = require("../build");
 const cors = require("cors");
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+const generateHtmlpage = require("../build/generateHtmlPage");
 require("dotenv").config();
 
 const copy = util.promisify(fs.copyFile);
+const read = util.promisify(fs.readFile);
+
+// this needs to be 50mb to stop the server from crapping itself
+// with a "request entity too large" error
+const urlencodedParser = bodyParser.urlencoded({
+	limit: "50mb",
+	extended: true,
+});
 
 // Create server
 const app = express();
@@ -18,24 +29,59 @@ const corsOptions = { origin: "*" };
 app.use(cors(corsOptions));
 
 // Support x-www-urlencoded on all routes
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencodedParser);
 
 // Start the server 🚀
 app.listen(process.env.PORT, () =>
 	debug(`app listening at http://localhost:${process.env.PORT}`)
 );
 
-app.post("/build", verifyPayload, (req, res) => {
+app.post("/build", [verifyPayload, urlencodedParser], (req, res) => {
 	build();
 	return res.status(200).json({ success: true });
 });
 
-app.post("/build/:id", verifyPayload, (req, res) => {
-	const jobs = [];
-	return res.status(200).json({ success: true });
+app.post("/build/:id", [verifyPayload, urlencodedParser], async (req, res) => {
+	debug(`building page ${req.params.id}`);
+
+	// get all the pages data
+	const pagesReq = await fetch(`http://10.10.10.12:8080/pages`);
+	const pages = await pagesReq.json();
+
+	// find my page in this data
+	const page = pages.filter((p) => {
+		if (p._id == req.params.id) {
+			return p;
+		}
+	})[0];
+
+	if (!page) {
+		return res.status(400).json({
+			success: false,
+			message: `tried to find page ${req.body.id} and failed to find it`,
+		});
+	}
+
+	let outputMarkdown = "";
+	for (i in page.source) {
+		const pageMarkdown = await read(`content/${page._id}_${i}.md`, "utf8");
+		outputMarkdown += `\n${pageMarkdown}\n`;
+	}
+
+	try {
+		generateHtmlpage(outputMarkdown, pages, { ...page });
+		debug(`finished building page ${page._id}!`);
+		return res.status(200).json({ success: true });
+	} catch (err) {
+		debug(`error building page ${page._id}!`);
+		return res.status(500).json({
+			success: false,
+			message: `failed to rebuild single page ${page._id}`,
+		});
+	}
 });
 
-app.post("/download", verifyPayload, (req, res) => {
+app.post("/download", [verifyPayload, urlencodedParser], (req, res) => {
 	debug("downloading", req.body.fileName);
 	// write the file
 	if (!fs.existsSync("content")) fs.mkdirSync("content");
