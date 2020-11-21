21017a04b5a6397da6633b15c780a6bec3e299cc
RolandWarburton <warburtonroland@gmail.com>
2020-11-21 20:18:41 +1100

    :construction: Change fetch history to align with 97e8729 from blogwatcher

--- reactFrontend2/src/components/helpers/fetchHistoryPromise.js
+++ reactFrontend2/src/components/helpers/fetchHistoryPromise.js
@@ -1,9 +1,5 @@
 export default async (_id) => {
-	// if in development then make the request against the dev server
-	const url =
-		process.env.NODE_ENV === "development"
-			? `http://devel:3000/api/history/find/${_id}`
-			: `/history/find/${_id}`;
+	const url = `https://watch.rolandw.dev/history/find/${_id}`;
 
 	console.log(`fetching items /history/find/${_id}`);
 	return fetch(url, { method: "get" });
