353edcaa462ced5f1ce3ae08706787cd34e28b6b
RolandWarburton <warburtonroland@gmail.com>
2021-03-21 15:52:11 +1100

    :recycle: Clean up getSiblings

--- app/build/buildSteps/getSiblings.js
+++ app/build/buildSteps/getSiblings.js
@@ -1,14 +1,21 @@
 const fetch = require("node-fetch");
 
-const getSiblings = async (websitePath) => {
-	const url = `${process.env.WATCHER_IP}/page?websitePath=/${websitePath}&level=1`;
+// level 0 = EXACT path only
+// level 1 = neighbors
+// level 2 = neighbors and neighbors children
+
+const getSiblings = async (websitePath, level) => {
+	// URL for the request
+	const url = `${process.env.WATCHER_IP}/page?websitePath=/${websitePath}&level=${level || 1}`;
+
+	// Request body
 	const options = {
 		method: "GET"
 	}
-	
+
+	// do the request
 	const request = await fetch(url, options);
 	const response = await request.json();
-	// console.log(await response)
 	return response;
 };
 
