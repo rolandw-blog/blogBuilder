333a59e5f2862edb54b1ff6524cda1d96075d2a3
RolandWarburton <warburtonroland@gmail.com>
2021-04-11 16:36:06 +1000

    :construction: Add "built on" section to nav

--- app/build/buildSteps/getDateData.js
+++ app/build/buildSteps/getDateData.js
@@ -16,15 +16,22 @@ const monthNames = [
 const getDateData = (templateData) => {
 	const { _id } = templateData;
 
-	const dateData = new Date(parseInt(_id.substring(0, 8), 16) * 1000);
+	// I am re-purposing this template field for the CURRENT date at build time
+	// 		instead of the time the page was created. To find out when the time was created, look under getModified.js
+	// 		getModified.js uses "History" documents logged when the page is created instead of the pages ID parsed to a string.
+	// const dateData = new Date(parseInt(_id.substring(0, 8), 16) * 1000);
+
+	// Get the current date
+	const dateData = new Date();
 
 	const createdDate = {
 		year: dateData.getFullYear(),
 		month: dateData.getMonth() + 1,
 		day: dateData.getDate(),
 		hour: dateData.getHours(),
-		full: `${monthNames[this.month]} ${this.day}, ${this.year}`, // pretty print full date
 	};
+
+	createdDate.full = `${monthNames[createdDate.month]} ${createdDate.day}, ${createdDate.year}`; // pretty print full date
 
 	return createdDate;
 };
